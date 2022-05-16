e66bd71c46ac614215ba8da110ea46827fa1b26a
Denis Rykovanov <denis.rykovanov@mail.ru>
2022-05-16 19:58:06 +0300

    added missed license

--- application/src/main/java/org/thingsboard/server/controller/converters/TopologyConverter.java
+++ application/src/main/java/org/thingsboard/server/controller/converters/TopologyConverter.java
@@ -1,17 +1,28 @@
+/**
+ * Copyright © 2016-2022 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.thingsboard.server.controller.converters;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.thingsboard.server.common.data.EntityType;
 import org.thingsboard.server.common.data.asset.Asset;
 import org.thingsboard.server.common.data.id.AssetId;
-import org.thingsboard.server.common.data.id.EntityIdFactory;
 import org.thingsboard.server.common.data.topology.dto.AssetWrapper;
-import org.thingsboard.server.common.data.topology.dto.Segments;
 import org.thingsboard.server.common.data.topology.dto.Territory;
 import org.thingsboard.server.controller.AssetController;
-
-import lombok.SneakyThrows;
 
 @Component
 public class TopologyConverter {
